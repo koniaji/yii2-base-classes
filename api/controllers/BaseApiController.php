d47d923709df962054086a7488cf2960051b9cab
Anton Morev <shikto@mail.ru>
2017-12-20 12:32:12 +0300

    cors

--- api/controllers/BaseApiController.php
+++ api/controllers/BaseApiController.php
@@ -9,8 +9,51 @@
 namespace Zvinger\BaseClasses\api\controllers;
 
 use yii\rest\Controller;
+use yii\web\BadRequestHttpException;
+use Zvinger\BaseClasses\api\filters\OptionsCorsFilter;
 
 class BaseApiController extends Controller
 {
+    public static $currentCorsOptions = [];
 
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $newBehaviors = [];
+        $baseOptions = [
+            'Origin'                           => ['*'],
+            'Access-Control-Allow-Credentials' => TRUE,
+            'Access-Control-Request-Method'    => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+            'Access-Control-Allow-Headers'     => ['*'],
+            'Access-Control-Request-Headers'   => ['*'],
+        ];
+        $newBehaviors['corsFilter'] = [
+            'class' => OptionsCorsFilter::className(),
+            'cors'  => array_merge($baseOptions, static::$currentCorsOptions),
+        ];
+
+        return array_merge($newBehaviors, $behaviors);
+    }
+
+
+    /**
+     * @param $key
+     * @param string $place post or get
+     * @return array|mixed
+     * @throws BadRequestHttpException
+     */
+    protected function checkAndGet($key, $place = 'post')
+    {
+        $request = \Yii::$app->request;
+        if ($place == 'post') {
+            $result = $request->post($key);
+        } elseif ($place == 'get') {
+            $result = $request->get($key);
+        }
+        if (empty($result)) {
+            throw new BadRequestHttpException($key . ' is empty');
+        }
+
+        return $result;
+    }
 }
---
+++ api/filters/OptionsCorsFilter.php
@@ -0,0 +1,20 @@
+<?php
+namespace Zvinger\BaseClasses\api\filters;
+
+use Yii;
+use yii\filters\Cors;
+
+class OptionsCorsFilter extends Cors
+{
+    public function beforeAction($action)
+    {
+        parent::beforeAction($action);
+
+        if (Yii::$app->getRequest()->getMethod() === 'OPTIONS') {
+            Yii::$app->getResponse()->getHeaders()->set('Allow', 'POST GET PUT');
+            Yii::$app->end();
+        }
+
+        return true;
+    }
+}
