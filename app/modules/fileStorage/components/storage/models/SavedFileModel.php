4459cddb90ac9474a9b0d025d3bfc05e40cc559c
Anton Morev <shikto@mail.ru>
2018-02-07 17:18:45 +0300

    wip

--- app/modules/fileStorage/components/storage/models/SavedFileModel.php
+++ app/modules/fileStorage/components/storage/models/SavedFileModel.php
@@ -10,6 +10,7 @@ namespace Zvinger\BaseClasses\app\modules\fileStorage\components\storage\models;
 
 use yii\base\BaseObject;
 use Zvinger\BaseClasses\app\modules\fileStorage\components\storage\storages\base\BaseVendorStorage;
+use Zvinger\BaseClasses\app\modules\fileStorage\components\storage\VendorFileStorageComponent;
 use Zvinger\BaseClasses\app\modules\fileStorage\models\fileStorageElement\FileStorageElementObject;
 
 class SavedFileModel extends BaseObject
@@ -20,13 +21,50 @@ class SavedFileModel extends BaseObject
     /** @var BaseVendorStorage */
     public $component;
 
+    /**
+     * @var VendorFileStorageComponent
+     */
+    private $_fileStorageComponent;
+
+    /**
+     * @return string
+     * @throws \Exception
+     */
     public function getFullUrl()
     {
-        $fullBase = $baseUrl = $this->component->getBaseUrl();
+        $fullBase = $baseUrl = $this->getComponent()->getBaseUrl();
         if (!empty($baseUrl)) {
             $fullBase = $baseUrl . DIRECTORY_SEPARATOR;
         }
 
         return $fullBase . $this->fileStorageElement->path;
+    }
+
+    /**
+     * @throws \Exception
+     * @return BaseVendorStorage
+     */
+    private function getComponent()
+    {
+        if (is_string($this->component)) {
+            if (empty($this->_fileStorageComponent)) {
+                throw new \Exception("No file storage component set");
+            }
+
+            $this->component = $this->_fileStorageComponent->getStorage($this->component);
+        }
+
+        return $this->component;
+    }
+
+    /**
+     * @param mixed $fileStorageComponent
+     * @return SavedFileModel
+     */
+    public function setFileStorageComponent($fileStorageComponent)
+    {
+        $this->_fileStorageComponent = $fileStorageComponent;
+
+        return $this;
     }
 }
