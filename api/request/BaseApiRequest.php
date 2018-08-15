2cf6e9240dd93b18388385ed53b02b51c4a15861
Anton Morev <shikto@mail.ru>
2018-08-15 12:37:07 +0300

    wip

--- api/request/BaseApiRequest.php
+++ api/request/BaseApiRequest.php
@@ -11,10 +11,38 @@ namespace Zvinger\BaseClasses\api\request;
 
 use Zvinger\BaseClasses\api\misc\BaseUserIncomeApiModel;
 
+/**
+ * Class BaseApiRequest
+ * @package Zvinger\BaseClasses\api\request
+ * @SWG\Definition()
+ */
 abstract class BaseApiRequest extends BaseUserIncomeApiModel
 {
     public static function getBaseData()
     {
         return \Yii::$app->request->post();
+    }
+
+    /**
+     * Дополнительная информация, которая не ложится в основную модель
+     * @var mixed
+     * @SWG\Property()
+     */
+    private $miscModelInformation;
+
+    /**
+     * @return mixed
+     */
+    public function getMiscModelInformation()
+    {
+        return $this->miscModelInformation;
+    }
+
+    /**
+     * @param mixed $miscModelInformation
+     */
+    public function setMiscModelInformation($miscModelInformation): void
+    {
+        $this->miscModelInformation = $miscModelInformation;
     }
 }
