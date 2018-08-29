655d0cc978d264870d8f40174979d753eaebea4b
Anton Morev <shikto@mail.ru>
2018-08-30 00:59:53 +0300

    Filter system

--- app/helpers/gii/ModelsGeneratorActions.php
+++ app/helpers/gii/ModelsGeneratorActions.php
@@ -9,6 +9,8 @@
 namespace Zvinger\BaseClasses\app\helpers\gii;
 
 use Zvinger\Auth\Mobsolutions\models\user\token\UserMobsolutionTokenObject;
+use Zvinger\BaseClasses\app\components\data\dictionary\models\DBDictionaryInfo;
+use Zvinger\BaseClasses\app\components\data\filterInfo\models\DBFilterMiscInfo;
 use Zvinger\BaseClasses\app\models\work\user\object\VendorUserObject;
 use Zvinger\BaseClasses\app\models\work\user\token\bearer\UserBearerTokenObject;
 
@@ -17,12 +19,18 @@ class ModelsGeneratorActions
     public static $globalNameSpaces = [
         'user'              => VendorUserObject::class,
         'user_bearer_token' => UserBearerTokenObject::class,
-        'user_mobsol_token' => UserMobsolutionTokenObject::class,
     ];
 
     public static function generateUserModels()
     {
         $nameSpaces = static::$globalNameSpaces;
+        if (class_exists('\Zvinger\Auth\Mobsolutions\models\user\token\UserMobsolutionTokenObject')) {
+            $nameSpaces['user_mobsol_token'] = '\Zvinger\Auth\Mobsolutions\models\user\token\UserMobsolutionTokenObject';
+        }
+        $nameSpaces = array_merge($nameSpaces, [
+            'dictionary_info'  => DBDictionaryInfo::class,
+            'filter_misc_info' => DBFilterMiscInfo::class,
+        ]);
         \Yii::$app->runAction('gii/model', [
             'tableName'         => 'user',
             'modelClass'        => 'DBUserObject',
@@ -41,6 +49,20 @@ class ModelsGeneratorActions
             'tableName'         => 'user_activation',
             'modelClass'        => 'DBUserActivationObject',
             'ns'                => 'Zvinger\BaseClasses\app\models\db\user\activation',
+            'generateRelations' => 'none',
+            'tablesNamespaces'  => $nameSpaces,
+        ]);
+        \Yii::$app->runAction('gii/model', [
+            'tableName'         => 'dictionary_info',
+            'modelClass'        => 'DBDictionaryInfo',
+            'ns'                => 'Zvinger\BaseClasses\app\components\data\dictionary\models',
+            'generateRelations' => 'none',
+            'tablesNamespaces'  => $nameSpaces,
+        ]);
+        \Yii::$app->runAction('gii/model', [
+            'tableName'         => 'filter_misc_info',
+            'modelClass'        => 'DBFilterMiscInfo',
+            'ns'                => 'Zvinger\BaseClasses\app\components\data\filterInfo\models',
             'generateRelations' => 'none',
             'tablesNamespaces'  => $nameSpaces,
         ]);
