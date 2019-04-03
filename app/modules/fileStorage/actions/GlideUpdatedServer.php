a2cbcd5c2df0979793013e70dc0ffe074ce80f22
Anton Morev <shikto@mail.ru>
2019-04-03 23:53:35 +0300

    wip

---
+++ app/modules/fileStorage/actions/GlideUpdatedServer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amorev
+ * Date: 28.03.2019
+ * Time: 19:43
+ */
+
+namespace Zvinger\BaseClasses\app\modules\fileStorage\actions;
+
+
+class GlideUpdatedServer extends \League\Glide\Server
+{
+
+}
